988212f62e57358e2dc59b3feed0d21c48a1ef58
Taaleena <106486147+Taaleena@users.noreply.github.com>
2022-09-25 21:25:48 +0300

    error

--- 1/dz7/import_date.py
+++
@@ -1,9 +0,0 @@
-def import_date (date, raz = None):
-    with open ("phone. csv", "a+") as file:
-        if raz == None:
-            for i in date:
-                file.write (f"{i}\n")
-            file.write (f"\n")
-        else:
-            file.write (raz.join(date))
-            file.write (f"\n")
--- 1/dz7/main.py
+++
@@ -1,4 +0,0 @@
-from secrets import choice
-from controller import *
-greeting ()
-choice_todo ()
